41e2712d5d43c7690b6a35033f0d6e2952037bed
Manoj Kamutam <144892109+manojkamutam@users.noreply.github.com>
2025-03-03 17:26:09 +0530

    Create outputs.tf

---
+++ VPC/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value       = aws_vpc.my_vpc.id
+}
+
+output "public_subnet_id" {
+  description = "The ID of the created public subnet"
+  value       = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  description = "The ID of the created private subnet"
+  value       = aws_subnet.private_subnet.id
+}
+
+output "security_group_id" {
+  description = "The ID of the created security group"
+  value       = aws_security_group.custom_sg.id
+}
